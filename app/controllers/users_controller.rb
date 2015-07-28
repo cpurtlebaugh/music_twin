eabf26447cbe6104339ec7f5374d573464bbda7e
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-07-27 19:40:38 -0700

    added sessions controller, user model, and db migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,29 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def edit
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def delete
+    @user = User.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+  params.require(:users).permit(:name, :email, :password_digest)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 resources :users
 resources :static_pages
+resources :sessions
 
 root 'static_pages#home'
 end
