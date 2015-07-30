a1cf86bc54d7399218d697ab3434702002303f25
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-07-30 00:45:53 -0700

    added helper_methods to detect login / logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :logged_in?
+  helper_method :log_in
+
+  # logs in new current user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # checks if current user is logged in, if not returns false
+  def logged_in?
+    !current_user.nil?
+  end
 
   private
 
