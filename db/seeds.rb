1d7a9b7cc9ef103e55b6907d2e0f61084dd18442
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-01-28 13:13:20 -0800

    change buttons to bootstrap styling, checked on api request, no longer available, update for scrolling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,89 +1,89 @@
 
 
-# require "httparty"
+require "httparty"
 
-# API_KEY = ENV["LASTFM_API_KEY"]
+API_KEY = ENV["LASTFM_API_KEY"]
 
 ## metros that are being defined / selected to make the api request on
 
-# GET_METROS_URI = "https://ws.audioscrobbler.com/2.0/?method=geo.getmetros"
-# COUNTRIES = [
-#   "United States",
-#   "Canada",
-#   "United Kingdom",
-#   "Mexico",
-#   "Australia",
-#   "Japan",
-#   "Germany",
-#   "France"
-# ]
+GET_METROS_URI = "https://ws.audioscrobbler.com/2.0/?method=geo.getmetros"
+COUNTRIES = [
+  "United States",
+  "Canada",
+  "United Kingdom",
+  "Mexico",
+  "Australia",
+  "Japan",
+  "Germany",
+  "France"
+]
 
 ## encoding the url response all to lowercase
-# def urize(value)
-#   URI.encode(value.downcase)
-# end
+def urize(value)
+  URI.encode(value.downcase)
+end
 
 ## ensures downcase for all returned data via api
-# def get_metro_uri(metro)
-#   GET_METROS_URI + "&country=#{urize(metro)}&api_key=#{API_KEY}&format=json"
-# end
+def get_metro_uri(metro)
+  GET_METROS_URI + "&country=#{urize(metro)}&api_key=#{API_KEY}&format=json"
+end
 
 ## request country metro list
-# def get_metro_list(country)
-#   value = HTTParty.get get_metro_uri(country)
-#   value["metros"]["metro"]
-# end
+def get_metro_list(country)
+  value = HTTParty.get get_metro_uri(country)
+  value["metros"]["metro"]
+end
 
 ## grabs metro list
-# def get_current_metro_list
-#   metros = []
-#   COUNTRIES.each do |country|
-#     metros = metros.concat(get_metro_list(country))
-#   end
-#   metros
-# end
+def get_current_metro_list
+  metros = []
+  COUNTRIES.each do |country|
+    metros = metros.concat(get_metro_list(country))
+  end
+  metros
+end
 
 # ------------------------------
 ## requests metro artists
-# GET_METRO_ARTISTS_URI = "http://ws.audioscrobbler.com/2.0/?method=geo.getmetroartistchart"
+GET_METRO_ARTISTS_URI = "http://ws.audioscrobbler.com/2.0/?method=geo.getmetroartistchart"
 
-# def get_metro_artist_uri(metro)
-#   GET_METRO_ARTISTS_URI + "&country=#{urize(metro["country"])}&metro=#{urize(metro["name"])}&api_key=#{API_KEY}&format=json"
-# end
+def get_metro_artist_uri(metro)
+  GET_METRO_ARTISTS_URI + "&country=#{urize(metro["country"])}&metro=#{urize(metro["name"])}&api_key=#{API_KEY}&format=json"
+end
 
-# def get_metro_artist_list(metro)
+def get_metro_artist_list(metro)
 
 # TODO: Washington DC, Montreal, Quebec and Mexico City all fail...
 #   #       Identify the problem!
 
 ## attempt to substitute failed cities
-#   # metro["name"].gsub! /Montreal/, "Montréal"
-#   # metro["name"].gsub! /Quebec/, "Québec"
+  metro["name"].gsub! /Montreal/, "Montréal"
+  metro["name"].gsub! /Quebec/, "Québec"
 
 ## specifies the failed cities if failed and stores top artists in artist variable
-#   value = HTTParty.get get_metro_artist_uri(metro)
-#   if value["topartists"]["artist"].nil?
-#     puts "  ERROR: Metro artist unavailable."
-#   else
-#     value = value["topartists"]["artist"]
-#     value.map do |artist|
-#       {
-#         :name => artist["name"],
-#         :mbid => artist["mbid"],
-#         # OPTIMIZE: should we add listeners here?
-#         :image_uri => artist["image"][2]["#text"]
-#       }
-#     end
-#   end
-# end
+  value = HTTParty.get get_metro_artist_uri(metro)
+  if value["topartists"]["artist"].nil?
+    puts "  ERROR: Metro artist unavailable."
+  else
+    value = value["topartists"]["artist"]
+    value.map do |artist|
+      {
+        :name => artist["name"],
+        :mbid => artist["mbid"],
+        # OPTIMIZE: should we add listeners here?
+        :image_uri => artist["image"][2]["#text"]
+      }
+    end
+  end
+end
 
 # ------------------------------
 ## loops through metro list and stores as metro, writes to yml file
-# metros = get_current_metro_list.map do |metro|
-#   puts metro["name"]
-#   metro["artists"] = get_metro_artist_list(metro)
-#   metro
-# end
+metros = get_current_metro_list.map do |metro|
+  puts metro["name"]
+  metro["artists"] = get_metro_artist_list(metro)
+  metro
+end
 
 # File.write("db/metros.yml", metros.to_yaml)
 
